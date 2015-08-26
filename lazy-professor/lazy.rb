898b3e5274890b3edd90b5d1eff227754cb2b989
Alan Voss <avoss@rent.com>
2015-08-26 10:36:17 -0700

    breaking out into multiple lines for better legibility

--- lazy-professor/lazy.rb
+++ lazy-professor/lazy.rb
@@ -1,2 +1,4 @@
-puts File.open('./test.txt').map { |x| x.split('') }.reduce([]) { |a,cs| cs.each_with_index { |c,i| a[i] ||= {}; a[i][c] ||= 0; a[i][c] += 1 }; a }
+puts File.open('./test.txt')
+  .map { |x| x.split('') }
+  .reduce([]) { |a,cs| cs.each_with_index { |c,i| a[i] ||= {}; a[i][c] ||= 0; a[i][c] += 1 }; a }
   .map { |cs| cs.max_by { |k,v| v } }.map(&:first).join('')
